b5f303bd330f55d5f93f0beb3608c3dbf4163a42
robeusanio11 <robeusanio11@gmail.com>
2023-02-04 16:10:02 -0800

    added api search for pasta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,29 @@
-export default function Home() {
+import API_KEY from '../config/API_KEY.js';
+
+export default function Home({recipes}) {
+  console.log(recipes);
   return (
-    <div>Hello World</div>
+    <>
+      <div>Search for Pasta Recipes:</div>
+      <ul>
+        {recipes.results.map((recipe) => (
+          <li key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image}/>
+          </li>
+        ))}
+      </ul>
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch/?apiKey=${API_KEY}&query=pasta`);
+  const recipes = await res.json();
+
+  return {
+    props: {
+      recipes
+    }
+  }
 }
