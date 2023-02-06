ece884a23ae82bad63a2fb32a36e74ece352c389
robeusanio11 <robeusanio11@gmail.com>
2023-02-05 21:20:02 -0800

    added bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import API_KEY from '../config/API_KEY.js';
 import APP_ID from '../config/APP_ID.js';
 import React, { useState } from 'react';
 
+// edanam api version
 export default function Home() {
   const [recipeList, setRecipeList] = useState([]);
   const [recipeQuery, setRecipeQuery] = useState('');
@@ -22,12 +23,26 @@ export default function Home() {
 
       <ul>
         {recipeList.map(({ recipe }) => (
+
           <li key={recipe.key}>
-              <h3>{recipe.label}</h3>
-              <img src={recipe.images.THUMBNAIL.url}/>
-            </li>
+            <div class="card">
+              <div class="card-body">
+                <h3 class="card-title"> {recipe.label}</h3>
+                <img src={recipe.images.THUMBNAIL.url} class="card-img-right"/>
+              </div>
+            </div>
+          </li>
         ))}
       </ul>
     </>
     );
   }
+
+{/* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */}
