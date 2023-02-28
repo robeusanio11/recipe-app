5c80c4c10ba6840ee0766bdba3d5ebdec9ec1799
robeusanio11 <robeusanio11@gmail.com>
2023-02-28 15:01:02 -0800

    small changes + like button

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function RecipeCard({recipe}) {
   return (
     <div key={recipe.key} class="card m-3" style={{minWidth: '18rem', maxHeight: '50rem'}} >
@@ -5,16 +7,15 @@ export default function RecipeCard({recipe}) {
       <div class="card-body" style={{height: '35rem'}}>
         <h5 class="card-title">{recipe.label}</h5>
         <p class="card-text"><small class="text-muted">{recipe.cuisineType[0]}</small></p>
-      <a class="btn btn-primary" type="button" href={recipe.url}>
-        See Recipe
-      </a>
-      <div>
-        <ul class="list-group list-group-flush" style={{maxHeight: '18rem', overflowY: 'scroll'}}>
-          {recipe.ingredients.map((ingredient) => (
-            <li class="list-group-item">{ingredient.food}</li>
-            ))}
-        </ul>
-            </div>
+        <a class="btn btn-primary" type="button" href={recipe.url}>
+          See Recipe
+        </a>
+        <a class="btn btn-outline-success m-2" type="button">Like</a>
+          <ul class="list-group list-group-flush" style={{maxHeight: '18rem', overflowY: 'scroll'}}>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index} class="list-group-item">{ingredient.food}</li>
+              ))}
+          </ul>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
 import RecipeCard from '../components/RecipeCard.js';
-import RecipeCarousel from '../components/RecipeCard.js';
 import NavBar from '../components/NavBar.js';
 // edanam api version
 export default function Home() {
   const [recipeList, setRecipeList] = useState([]);
-  const [recipeQuery, setRecipeQuery] = useState('');
 
     return (
       <>
